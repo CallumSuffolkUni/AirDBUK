488c8faa86869542063337e009a7bb390b3a89ef
CallumSuffolkUni <callum07467@gmail.com>
2026-03-24 15:48:26 +0000

    settings change

--- AirDBUK/AirDBUK/settings.py
+++ AirDBUK/AirDBUK/settings.py
@@ -132,6 +132,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+STATIC_ROOT = BASE_DIR / 'staticfiles'  # where collectstatic dumps files
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
