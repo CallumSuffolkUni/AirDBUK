1d7c9b2c4284756e6347c1556d8a29b17f0e82ec
CallumSuffolkUni <callum07467@gmail.com>
2026-03-24 15:27:28 +0000

    debug set to true

--- AirDBUK/AirDBUK/settings.py
+++ AirDBUK/AirDBUK/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-evmww6ll4#6_5o5bi5rzxorsq09ef6n**$wdrg*9t(bzqgw_(6'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['airdbuk.onrender.com']
 
