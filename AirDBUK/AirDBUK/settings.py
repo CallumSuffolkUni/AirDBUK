7d2fb6da51d872a01ef9bbbd0784599d7d863dd1
CallumSuffolkUni <callum07467@gmail.com>
2026-03-24 15:44:58 +0000

    settings change

--- AirDBUK/AirDBUK/settings.py
+++ AirDBUK/AirDBUK/settings.py
@@ -52,6 +52,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'AirDBUK.urls'
@@ -132,3 +133,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
